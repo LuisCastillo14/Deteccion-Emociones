0c9159228b5a7e749724845068eb9dd710a2e2ef
LuisCastillo14 <luisangelcastilloplasencia@gmail.com>
2025-11-12 15:33:55 -0500

    Act3. api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -10,6 +10,7 @@ console.log("API URL:", API_URL);
 // ---------- Análisis de imagen ----------
 export const analyzeImage = async (file: File) => {
   const fd = new FormData();
+  console.log("Conectando a API para analizar", API_URL);
   fd.append("file", file);
   const { data } = await axios.post(`${API_URL}/analyze-image`, fd, {
     headers: { "Content-Type": "multipart/form-data" },
@@ -20,11 +21,13 @@ export const analyzeImage = async (file: File) => {
 // ---------- Análisis de frame ----------
 export const analyzeFrame = async (blob: Blob) => {
   const fd = new FormData();
+  console.log("Conectando a API para analizar", API_URL);
   fd.append("file", blob, "frame.jpg");
   const { data } = await axios.post(`${API_URL}/analyze-frame`, fd, {
     headers: { "Content-Type": "multipart/form-data" },
     timeout: 10000,
   });
+  
   return data as {
     num_faces: number;
     results: { id: number; bbox: number[]; emotion: string; confidence: number }[];
@@ -33,9 +36,11 @@ export const analyzeFrame = async (blob: Blob) => {
 
 // ---------- NUEVO: Análisis de video de YouTube ----------
 export const analyzeYouTube = async (youtubeUrl: string) => {
+  console.log("Conectando a API para analizar video de YouTube:", API_URL);
   const { data } = await axios.post(`${API_URL}/analyze-youtube`, {
     url: youtubeUrl, 
   });
+
   return data as {
     status: string;
     public_url: string;
