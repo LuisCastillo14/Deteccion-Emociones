4b6e32e89d611421ba1ba754675169f5773cb427
LuisCastillo14 <luisangelcastilloplasencia@gmail.com>
2025-11-17 15:44:15 -0500

    Correccion logos

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
     <nav className="navbar navbar-custom px-3 d-flex justify-content-between align-items-center">
       <div className="d-flex align-items-center">
         <img
-          src="logo.png"
+          src="/logo.png" 
           alt="EmotiScan"
           style={{ width: "30px", marginRight: "10px" }}
         />
